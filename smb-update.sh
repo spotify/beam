e8fa78e76a7b761ceef1a9e440d6b1700589fefb
Neville Li <neville@spotify.com>
2019-07-08 16:55:29 -0400

    add smb-update.sh

---
+++ smb-update.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e -u -f -o pipefail
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $(basename $0) SRC_BRANCH DST_BRANCH"
+    exit 0
+fi
+
+SRC=$1
+DST=$2
+
+echo "Updating master"
+git fetch origin
+git fetch upstream
+git checkout master
+git reset --hard upstream/master
+git push
+
+echo "Updating source branch $SRC"
+git checkout $SRC
+git rebase upstream/master
+git push --force
+
+echo "Updating destination branch $DST"
+git checkout $DST
+git reset --hard origin/$DST
+git rebase upstream/master
+
+echo "Updating files"
+for FILE in $(git diff upstream/master --name-only); do
+    git checkout $SRC -- $FILE
+done
+
+echo "Applying Spotless"
+./gradlew :sdks:java:extensions:smb:spotlessApply
+
+echo "Building module"
+./gradlew -p sdks/java/extensions/smb build
+
+echo "Committing changes"
+git commit -m "Update changes from WIP branch"
+
+git show HEAD
