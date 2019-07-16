443d70838545d8fb9b18ea0dbc7437d5cfb99ada
Neville Li <neville@spotify.com>
2019-07-16 17:39:06 -0400

    add compression suffix

--- sdks/java/extensions/smb/src/main/java/org/apache/beam/sdk/extensions/smb/SortedBucketSource.java
+++ sdks/java/extensions/smb/src/main/java/org/apache/beam/sdk/extensions/smb/SortedBucketSource.java
@@ -365,7 +365,9 @@ public class SortedBucketSource<FinalKeyT>
       // which buckets should be merged together for the join.
       for (int i = bucketId; i < numBucketsInSource; i += leastNumBuckets) {
         for (int j = 0; j < numShards; j++) {
-          final ResourceId file = fileAssignment.forBucket(BucketShardId.of(i, j), metadata);
+          final ResourceId file =
+              fileAssignment.forBucket(
+                  BucketShardId.of(i, j), metadata, fileOperations.getCompression());
           try {
             iterators.add(fileOperations.iterator(file));
           } catch (Exception e) {
