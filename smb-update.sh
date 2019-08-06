5d9dd9b60c4b2a62a0a6d55f12d5dde4e27f2d4c
Neville Li <neville@spotify.com>
2019-08-06 15:57:57 -0400

    update smb-update.sh

--- smb-update.sh
+++ smb-update.sh
@@ -2,44 +2,33 @@
 
 set -e -u -f -o pipefail
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $(basename $0) SRC_BRANCH DST_BRANCH"
-    exit 0
-fi
+SRC=smb-wip
 
-SRC=$1
-DST=$2
+for DST in smb-metadata smb-fileops smb-sink smb-source smb-api smb-benchmark; do
+    echo "============================================================"
+    echo "Updating branch $DST"
 
-#echo "Updating master"
-#git fetch origin
-#git fetch upstream
-#git checkout master
-#git reset --hard upstream/master
-#git push
-#
-#echo "Updating source branch $SRC"
-#git checkout $SRC
-#git rebase upstream/master
-#git push --force
-#
-#echo "Updating destination branch $DST"
-#git checkout $DST
-#git reset --hard origin/$DST
-#git rebase upstream/master
+    git checkout $DST
+    echo "Updating files"
+    for FILE in $(git diff master --name-only); do
+        git checkout $SRC -- $FILE
+    done
 
-git checkout $DST
-echo "Updating files"
-for FILE in $(git diff master --name-only); do
-    git checkout $SRC -- $FILE
+    if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
+        echo "Nothing to commit"
+    else
+        echo "Applying Spotless"
+        ./gradlew :sdks:java:extensions:smb:spotlessApply
+
+        echo "Building module"
+        ./gradlew -p sdks/java/extensions/smb build
+
+        echo "Committing changes"
+        git commit -m "Update changes from WIP branch" || true
+
+        git show HEAD
+        git push
+    fi
 done
 
-echo "Applying Spotless"
-./gradlew :sdks:java:extensions:smb:spotlessApply
-
-echo "Building module"
-./gradlew -p sdks/java/extensions/smb build
-
-echo "Committing changes"
-git commit -m "Update changes from WIP branch"
-
-git show HEAD
+git checkout $SRC
